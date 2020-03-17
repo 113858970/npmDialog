ba7e857f3e7eca37bfbdf9e11dac8b47a99fcf80
林金龙 <113858970@qq.com>
2020-03-17 17:13:19 +0800

    修改package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require("path");
 const webpack = require("webpack");
 const uglify = require("uglifyjs-webpack-plugin");
- 
+
 module.exports = {
     devtool: 'source-map',
     entry: "./src/index.js",//入口文件，就是上步骤的src目录下的index.js文件，
     output: {
         path: path.resolve(__dirname, './dist'),//输出路径，就是上步骤中新建的dist目录，
         publicPath: '/dist/',
-        filename: 'pdkModal.min.js',
+        filename: 'npmDalog.min.js',
         libraryTarget: 'umd',
         umdNamedDefine: true
     },
